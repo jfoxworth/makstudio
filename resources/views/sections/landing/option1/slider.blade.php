b7d791e95604c0203de12c0da6e55b505def5a59
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-22 10:45:40 -0500

    Initial build

--- resources/views/sections/landing/option1/slider.blade.php
+++ resources/views/sections/landing/option1/slider.blade.php
@@ -5,12 +5,17 @@
 		<div class="full-screen force-full-screen" style="background: url('https://makstudio.s3.us-east-2.amazonaws.com/MakWall1.jpg') center center no-repeat; background-size: cover;">
 
 			<div class="container clearfix">
+
 				<div class="emphasis-title vertical-middle center">
+
 					<img src="/images/makTextWhite.png" width="200px">
+
+					<div class="heading-block title-center nobottomborder">
+						<span>Custom made architectural walls, desks, and more</span>
+					</div>
+
 				</div>
-				<div class="heading-block title-center nobottomborder">
-					<span>Custom made architectural walls, desks, and more</span>
-				</div>
+
 			</div>
 
 		</div>
