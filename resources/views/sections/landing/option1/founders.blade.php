2da176efdcea31e2e2ed2e237331668c137630a2
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-22 14:00:01 -0500

    Initial build

--- resources/views/sections/landing/option1/founders.blade.php
+++ resources/views/sections/landing/option1/founders.blade.php
@@ -7,67 +7,65 @@
 	</div>
 
 
-						<div class="row">
+	<div class="row">
 
-							<div class="col-lg-3 col-md-6 bottommargin"></div>
+		<div class="col-lg-3 col-md-6 bottommargin"></div>
 
-							<div class="col-lg-3 col-md-6 bottommargin">
+		<div class="col-lg-3 col-md-6 bottommargin">
 
-								<div class="team">
-									<div class="team-image">
-										<img src="images/team/2.jpg" alt="Josh Clark">
-									</div>
-									<div class="team-desc team-desc-bg">
-										<div class="team-title"><h4>Liz Cordil</h4><span>Co-Founder, AIA, RID</span></div>
-										<a href="#" class="social-icon inline-block si-small si-light si-rounded si-facebook">
-											<i class="icon-facebook"></i>
-											<i class="icon-facebook"></i>
-										</a>
-										<a href="#" class="social-icon inline-block si-small si-light si-rounded si-twitter">
-											<i class="icon-twitter"></i>
-											<i class="icon-twitter"></i>
-										</a>
-										<a href="#" class="social-icon inline-block si-small si-light si-rounded si-gplus">
-											<i class="icon-gplus"></i>
-											<i class="icon-gplus"></i>
-										</a>
-									</div>
-								</div>
-
-							</div>
-
-							<div class="col-lg-3 col-md-6 bottommargin">
-
-								<div class="team">
-									<div class="team-image">
-										<img src="images/team/8.jpg" alt="Mary Jane">
-									</div>
-									<div class="team-desc team-desc-bg">
-										<div class="team-title"><h4>Jose Aguilar</h4><span>Co-Founder, Master Fabricator, AIA</span></div>
-										<a href="#" class="social-icon inline-block si-small si-light si-rounded si-facebook">
-											<i class="icon-facebook"></i>
-											<i class="icon-facebook"></i>
-										</a>
-										<a href="#" class="social-icon inline-block si-small si-light si-rounded si-twitter">
-											<i class="icon-twitter"></i>
-											<i class="icon-twitter"></i>
-										</a>
-										<a href="#" class="social-icon inline-block si-small si-light si-rounded si-gplus">
-											<i class="icon-gplus"></i>
-											<i class="icon-gplus"></i>
-										</a>
-									</div>
-								</div>
-
-							</div>
-
-							<div class="col-lg-3 col-md-6 bottommargin"></div>
-
-						</div>
-
-					</div>
+			<div class="team">
+				<div class="team-image">
+					<img src="https://makstudio.s3.us-east-2.amazonaws.com/lizCordil.png" alt="Liz Cordil">
 				</div>
+				<div class="team-desc team-desc-bg">
+					<div class="team-title"><h4>Liz Cordil</h4><span>Co-Founder, AIA, RID</span></div>
+					<a href="#" class="social-icon inline-block si-small si-light si-rounded si-facebook">
+						<i class="icon-facebook"></i>
+						<i class="icon-facebook"></i>
+					</a>
+					<a href="#" class="social-icon inline-block si-small si-light si-rounded si-twitter">
+						<i class="icon-twitter"></i>
+						<i class="icon-twitter"></i>
+					</a>
+					<a href="#" class="social-icon inline-block si-small si-light si-rounded si-gplus">
+						<i class="icon-gplus"></i>
+						<i class="icon-gplus"></i>
+					</a>
+				</div>
+			</div>
 
+		</div>
+
+		<div class="col-lg-3 col-md-6 bottommargin">
+
+			<div class="team">
+				<div class="team-image">
+					<img src="https://makstudio.s3.us-east-2.amazonaws.com/joseAguilar.png" alt="Jose Aguilar">
+				</div>
+				<div class="team-desc team-desc-bg">
+					<div class="team-title"><h4>Jose Aguilar</h4><span>Co-Founder, Master Fabricator, AIA</span></div>
+					<a href="#" class="social-icon inline-block si-small si-light si-rounded si-facebook">
+						<i class="icon-facebook"></i>
+						<i class="icon-facebook"></i>
+					</a>
+					<a href="#" class="social-icon inline-block si-small si-light si-rounded si-twitter">
+						<i class="icon-twitter"></i>
+						<i class="icon-twitter"></i>
+					</a>
+					<a href="#" class="social-icon inline-block si-small si-light si-rounded si-gplus">
+						<i class="icon-gplus"></i>
+						<i class="icon-gplus"></i>
+					</a>
+				</div>
+			</div>
+
+		</div>
+
+		<div class="col-lg-3 col-md-6 bottommargin"></div>
+
+	</div>
+
+</section>
 
 
 
