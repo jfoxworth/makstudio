b788e2748ad48192843bffeaa375063b90896739
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-22 16:48:12 -0500

    Initial build

--- resources/views/sections/contactUs/contactInfo.blade.php
+++ resources/views/sections/contactUs/contactInfo.blade.php
@@ -3,12 +3,12 @@
 	<div class="nopadding row noleftmargin norightmargin bottommargin-lg align-items-stretch">
 		<div id="headquarters-map" class="col-lg-8 col-md-6 gmap d-none d-md-block">
 			<a href="https://www.google.com/maps/place/305+Velasco+St,+Houston,+TX+77003/@29.7506327,-95.3409686,13z/data=!4m5!3m4!1s0x8640bee7fbf303b5:0x8c2d611075a46a94!8m2!3d29.7504088!4d-95.3419932">
-				<img src="https://makstudio.s3.us-east-2.amazonaws.com/makStudioMap.png" style="width:100%";>
+				<img src="https://makstudio.s3.us-east-2.amazonaws.com/makStudioMap.png" style="padding-right:0px; padding-left:0px; width:100%";>
 			</a>
 		</div>
-		<div class="col-lg-4 col-md-6" style="background-color: #222;">
+		<div class="col-lg-4 col-md-6" style="background-color: #222; color:#FFFFFF">
 			<div class="col-padding">
-				<h3 class="font-body t400 ls1">Our Headquarters</h3>
+				<h3 class="font-body t400 ls1">Mak Studion Location</h3>
 
 				<div style="font-size: 16px; line-height: 1.7;">
 					<address style="line-height: 1.7;">
@@ -17,13 +17,6 @@
 						San Francisco, CA 94107<br>
 					</address>
 
-					<div class="clear topmargin-sm"></div>
-
-					<address style="line-height: 1.7;">
-						<strong>Europe:</strong><br>
-						795 Folsom Ave, Suite 600<br>
-						San Francisco, CA 94107<br>
-					</address>
 
 					<div class="clear topmargin"></div>
 
