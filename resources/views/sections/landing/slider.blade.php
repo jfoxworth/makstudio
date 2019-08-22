6cb5fd24fec3890f83cc79e1a164a9f5d246fcee
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-22 09:33:54 -0500

    Initial build

---
+++ resources/views/sections/landing/slider.blade.php
@@ -0,0 +1,44 @@
+		<div id="home" class="page-section" style="position:absolute;top:0;left:0;width:100%;height:200px;z-index:-2;"></div>
+
+		<section id="slider" class="slider-element slider-parallax full-screen with-header swiper_wrapper clearfix">
+
+			<div class="slider-parallax-inner">
+
+				<div class="swiper-container swiper-parent">
+					<div class="swiper-wrapper">
+
+						<div class="swiper-slide dark d-none d-md-block d-lg-block d-xl-block" style="background-image: url('https://makstudio.s3.us-east-2.amazonaws.com/MakWall1.jpg');">
+							<div class="container clearfix">
+								<div class="vertical-middle dark center">
+
+									<div class="col_half center">
+										<div class="center logoBox">
+											<div class="secondaryLogoText">Mak Studio</div>
+										</div>
+									</div>
+
+								</div>
+							</div>
+						</div>
+
+						<div class="swiper-slide dark d-block d-md-none" style="background-image: url('https://makstudio.s3.us-east-2.amazonaws.com/MakWall1.jpg');">
+							<div class="container clearfix">
+								<div class="vertical-middle dark center notoppadding notopmargin">
+
+									<div class="col_half center">
+										<div class="center logoBox">
+											<div class="secondaryLogoTextSmall">Mak Studio</div>
+										</div>
+									</div>
+
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+
+			</div>
+
+		</section>
+
