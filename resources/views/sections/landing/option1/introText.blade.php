260b5844b5d0f7e28384d3a1cc98c4b9279198cb
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-23 03:25:24 -0500

    Initial build

--- resources/views/sections/landing/option1/introText.blade.php
+++ resources/views/sections/landing/option1/introText.blade.php
@@ -4,8 +4,6 @@
 
 		<div class="divcenter"><img src="https://makstudio.s3.us-east-2.amazonaws.com/logoBlack.png" width="250px"></div>
 
-		<h2 class="divcenter bottommargin font-body" style="max-width: 700px; font-size: 40px;">Giving you the freedom and creativity to make cool stuff</h2>
-
 		<div class="clear"></div>
 
 	</div>
---
+++ resources/views/sections/landing/option1/sloganBreak.blade.php
@@ -0,0 +1,5 @@
+<div class="section dark nomargin">
+	<div class="divcenter center" style="max-width: 900px;">
+		<h2 class="nobottommargin t300 ls1">Giving you the freedom and creativity to make cool stuff</h2>
+	</div>
+</div>
