8f03e558f91366b62c6602bd7f6967054a52257d
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-23 00:31:33 -0500

    Initial build

--- resources/views/sections/products/productList.blade.php
+++ resources/views/sections/products/productList.blade.php
@@ -1,10 +1,12 @@
+
+<!-- Architectural Slat Wall -->
 <div class="section nobg notopmargin noborder bottommargin-sm">
 
 	<div class="container clearfix">
 
 		<div class="col_half nobottommargin">
 
-			<div class="heading-block bottommargin-sm" style="padding-top: 40px;">
+			<div class="heading-block bottommargin-sm" style="padding-top: 20px;">
 
 				<h2>Architectural Slat Walls</h2>
 				<span>Beautiful, made to order walls of any shape and size</span>
@@ -27,6 +29,7 @@
 
 
 
+<!-- Faceted Wall -->
 <div class="section nobg topmargin-lg nobottommargin">
 
 	<div class="container clearfix">
@@ -37,7 +40,7 @@
 
 		<div class="col_half nobottommargin col_last">
 
-			<div class="heading-block bottommargin-sm" style="padding-top: 40px;">
+			<div class="heading-block bottommargin-sm" style="padding-top: 20px;">
 
 				<h2>Faceted Walls</h2>
 				<span>Our faceted walls define any office space</span>
@@ -45,6 +48,138 @@
 			</div>
 
 			<p>Faceted walls add depth and texture to any room and define a working or entertainment space. Our tools let you design your own custom wall that we build and install.</p>
+
+			<a href="#" class="button button-border button-large button-rounded notopmargin noleftmargin">Customize Your Own</a>
+
+		</div>
+
+	</div>
+
+</div>
+
+
+
+
+
+<!-- Desks -->
+<div class="section nobg notopmargin noborder bottommargin-sm">
+
+	<div class="container clearfix">
+
+		<div class="col_half nobottommargin">
+
+			<div class="heading-block bottommargin-sm" style="padding-top: 20px;">
+
+				<h2>Custom Desks</h2>
+				<span>Unique and custom made desks</span>
+
+			</div>
+
+			<p>We custom make desks for reception areas and offices. Contact our office to get more information on what we can do and take a look at our <a href="/ourWork">previous work.</a></p>
+
+			<a href="#" class="button button-border button-large button-rounded notopmargin noleftmargin">Desk Options</a>
+
+		</div>
+
+		<div class="col_half bottommargin-sm center col_last">
+			<img src="https://makstudio.s3.us-east-2.amazonaws.com/deskDemo.png" alt="Custom Desk">
+		</div>
+
+	</div>
+</div>
+
+
+
+
+
+
+<!-- Planter Wall -->
+<div class="section nobg topmargin-lg nobottommargin">
+
+	<div class="container clearfix">
+
+		<div class="col_half nobottommargin center">
+			<img src="https://makstudio.s3.us-east-2.amazonaws.com/flowerWallDemo.jpg" alt="Planter Wall" >
+		</div>
+
+		<div class="col_half nobottommargin col_last">
+
+			<div class="heading-block bottommargin-sm" style="padding-top: 20px;">
+
+				<h2>Planter Walls</h2>
+				<span>Our faceted walls define any office space</span>
+
+			</div>
+
+			<p>Faceted walls add depth and texture to any room and define a working or entertainment space. Our tools let you design your own custom wall that we build and install.</p>
+
+			<a href="#" class="button button-border button-large button-rounded notopmargin noleftmargin">Customize Your Own</a>
+
+		</div>
+
+	</div>
+
+</div>
+
+
+
+
+
+
+
+
+
+<!-- Back Lit Wall -->
+<div class="section nobg notopmargin noborder bottommargin-sm">
+
+	<div class="container clearfix">
+
+		<div class="col_half nobottommargin">
+
+			<div class="heading-block bottommargin-sm" style="padding-top: 20px;">
+
+				<h2>Back Lit Walls</h2>
+				<span>Back lit walls provide a unique look and feel for corporate reception areas</span>
+
+			</div>
+
+			<p>Our back lit walls are custom made and can be installed whereever needed</p>
+
+			<a href="#" class="button button-border button-large button-rounded notopmargin noleftmargin">Design Your Wall</a>
+
+		</div>
+
+		<div class="col_half bottommargin-sm center col_last">
+			<img src="https://makstudio.s3.us-east-2.amazonaws.com/backlitDemo.jpg" alt="Back Lit Wall">
+		</div>
+
+	</div>
+</div>
+
+
+
+
+
+
+<!-- Planter Wall -->
+<div class="section nobg topmargin-lg nobottommargin">
+
+	<div class="container clearfix">
+
+		<div class="col_half nobottommargin center">
+			<img src="https://makstudio.s3.us-east-2.amazonaws.com/flowerWallDemo.jpg" alt="Planter Wall" >
+		</div>
+
+		<div class="col_half nobottommargin col_last">
+
+			<div class="heading-block bottommargin-sm" style="padding-top: 20px;">
+
+				<h2>Planter Walls</h2>
+				<span>Planter walls provide a unique way to add comfort and warmth to you office</span>
+
+			</div>
+
+			<p>We can design and install any custom planter wall for you office or reception area.</p>
 
 			<a href="#" class="button button-border button-large button-rounded notopmargin noleftmargin">Customize Your Own</a>
 
