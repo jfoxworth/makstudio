5108695b47b4f0ef4fe96a381a4a8fd2d70d4173
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-24 14:32:31 -0500

    Initial build

--- resources/views/sections/ourWork/workList.blade.php
+++ resources/views/sections/ourWork/workList.blade.php
@@ -8,10 +8,108 @@
 
 	<div class="container clearfix">
 		
-	<div class="fancy-title title-dotted-border title-center">
-		<h2>Eighth Wonder</h2>
-		<span>This is text describing the work</span>
-	</div>
+
+		<!-- Eighth Wonder Bar -->
+		<div class="fancy-title title-dotted-border title-center">
+			<h2>Eighth Wonder</h2>
+		</div>
+	
+		<div class="heading-block">
+			<span>This is text describing the work</span>
+		</div>
+
+
+		<div class="masonry-thumbs grid-5 clearfix" 
+			 data-big="3" 
+			 data-lightbox="gallery" 
+			 style="position: relative; height: 200px;">
+			
+			<a href="https://makstudio.s3.us-east-2.amazonaws.com/EighthWonder1.jpg" 
+			   data-lightbox="gallery-item" 
+			   style="width: 133px; position: absolute; left: 0px; top: 0px;">
+				
+				<img src="https://makstudio.s3.us-east-2.amazonaws.com/EighthWonder1.jpg" alt="Eighth Wonder Image">
+				<div class="overlay">
+					<div class="overlay-wrap">
+						<i class="icon-line-plus"></i>
+					</div>
+				</div>
+			</a>
+
+			<a href="https://makstudio.s3.us-east-2.amazonaws.com/EighthWonder2.jpg" 
+			   data-lightbox="gallery-item" 
+			   style="width: 133px; position: absolute; left: 133px; top: 0px;">
+			
+				<img src="https://makstudio.s3.us-east-2.amazonaws.com/EighthWonder6.jpg" alt="Eighth Wonder Image">
+				<div class="overlay">
+					<div class="overlay-wrap">
+						<i class="icon-line-plus"></i>
+					</div>
+				</div>
+			</a>
+
+			<a href="https://makstudio.s3.us-east-2.amazonaws.com/EighthWonder2.jpg" 
+			   data-lightbox="gallery-item" 
+			   style="width: 266px; position: absolute; left: 266px; top: 0px;">
+			
+				<img src="https://makstudio.s3.us-east-2.amazonaws.com/EighthWonder6.jpg" alt="Eighth Wonder Image">
+				<div class="overlay">
+					<div class="overlay-wrap">
+						<i class="icon-line-plus"></i>
+					</div>
+				</div>
+			</a>
+
+			<a href="https://makstudio.s3.us-east-2.amazonaws.com/EighthWonder3.jpg" 
+			   data-lightbox="gallery-item" 
+			   style="width: 133px; position: absolute; left: 532px; top: 0px;">
+			
+				<img src="https://makstudio.s3.us-east-2.amazonaws.com/EighthWonder3.jpg" alt="Eighth Wonder Image">
+				<div class="overlay">
+					<div class="overlay-wrap">
+						<i class="icon-line-plus"></i>
+					</div>
+				</div>
+			</a>
+
+			<a href="https://makstudio.s3.us-east-2.amazonaws.com/EighthWonder4.jpg" 
+			   data-lightbox="gallery-item" 
+			   style="width: 133px; position: absolute; left: 0px; top: 100px;">
+			
+				<img src="https://makstudio.s3.us-east-2.amazonaws.com/EighthWonder4.jpg" alt="Eighth Wonder Image">
+				<div class="overlay">
+					<div class="overlay-wrap">
+						<i class="icon-line-plus"></i>
+					</div>
+				</div>
+			</a>
+
+			<a href="https://makstudio.s3.us-east-2.amazonaws.com/EighthWonder5.jpg" 
+			   data-lightbox="gallery-item" 
+			   style="width: 133px; position: absolute; left: 133px; top: 100px;">
+			
+				<img src="https://makstudio.s3.us-east-2.amazonaws.com/EighthWonder5.jpg" alt="Eighth Wonder Image">
+				<div class="overlay">
+					<div class="overlay-wrap">
+						<i class="icon-line-plus"></i>
+					</div>
+				</div>
+			</a>
+
+			<a href="https://makstudio.s3.us-east-2.amazonaws.com/EighthWonder6.jpg" 
+			   data-lightbox="gallery-item" 
+			   style="width: 133px; position: absolute; left: 532px; top: 100px;">
+			
+				<img src="https://makstudio.s3.us-east-2.amazonaws.com/EighthWonder6.jpg" alt="Eighth Wonder Image">
+				<div class="overlay">
+					<div class="overlay-wrap">
+						<i class="icon-line-plus"></i>
+					</div>
+				</div>
+			</a>
+
+		</div>
+
 
 
 </section>
