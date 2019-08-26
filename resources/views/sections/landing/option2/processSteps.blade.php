fb947b764dc7fe4db4d9db1a27763c6378a6c860
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-26 13:14:32 -0500

    Initial build

--- resources/views/sections/landing/option2/processSteps.blade.php
+++ resources/views/sections/landing/option2/processSteps.blade.php
@@ -6,7 +6,7 @@
 			<h2 class="text-title-light text-dark mb-5 ls1 uppercase">We handle all steps</h2>
 		</div>
 
-		<ul class="process-steps bottommargin clearfix">
+		<ul class="process-steps process-3 bottommargin clearfix">
 
 			<li>
 				<a href="#" class="i-circled i-alt divcenter">1</a>
@@ -24,6 +24,37 @@
 			</li>
 
 		</ul>
+
+
+		<div class="row">
+
+			<div class="col_one_third center">
+
+				<div class="center"><img src="https://makstudio.s3.us-east-2.amazonaws.com/steps1.png" width="150px"></div>
+
+				<p>Our custom tools let you design and price your own custom slat walls or ceiling</p>
+
+			</div>
+
+
+			<div class="col_one_third">
+
+				<div class="center"><img src="https://makstudio.s3.us-east-2.amazonaws.com/steps1.png" width="150px"></div>
+
+				<p>mak studios can build and install your design</p>
+
+			</div>
+
+
+			<div class="col_one_third col_last">
+
+				<div class="center"><img src="https://makstudio.s3.us-east-2.amazonaws.com/steps1.png" width="150px"></div>
+
+				<p>Once installed, you can enjoy a totally unique space that is customed designed for your company</p>
+
+			</div>
+
+		</div>
 
 	</div>
 
