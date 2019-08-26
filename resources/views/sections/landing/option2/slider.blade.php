19d3beec5493cb243f308a2c8467fa609ae5d81b
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-26 10:02:39 -0500

    Initial build

---
+++ resources/views/sections/landing/option2/slider.blade.php
@@ -0,0 +1,26 @@
+<section id="slider" class="slider-element slider-parallax full-screen with-header force-full-screen clearfix">
+
+	<div class="slider">
+
+		<div class="full-screen force-full-screen" style="background: url('https://makstudio.s3.us-east-2.amazonaws.com/MakWall1.jpg') center center no-repeat; background-size: cover;">
+
+			<div class="container clearfix">
+
+				<div class="emphasis-title vertical-middle center">
+
+					<img src="/images/makTextWhite.png" width="200px">
+
+					<div class="heading-block title-center nobottomborder topmargin">
+						<h4 class="whiteText">Custom made architectural walls, desks, and more</h4>
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+	</div>
+
+</section>
+
+
---
+++ resources/views/sections/landing/option2/workofartBreak2.blade.php
@@ -0,0 +1,17 @@
+<div class="section parallax dark notopmargin bottommargin-lg noborder" 
+	 style="padding: 150px 0; background-size:cover; background-image: url('https://makstudio.s3.us-east-2.amazonaws.com/parrallax2.jpg');" 
+	 data-bottom-top="background-position:0px 0px;" 
+	 data-top-bottom="background-position:0px -300px;">
+	
+	<div class="container center clearfix">
+
+		<div class="emphasis-title">
+			<h2>Brand Your Business</h2>
+			<p class="lead topmargin-sm">Make a statement with your company's entrance and reception area.</p>
+		</div>
+
+		<a href="/products" class="button button-border button-rounded button-light button-large">Product List</a>
+		<!-- <a href="#" class="button button-rounded button-white button-light button-large">Buy Now</a> -->
+
+	</div>
+</div>
