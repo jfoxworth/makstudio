cb8b2f843bea8c4cb6d2f8d7fa0f3ff4f2a6e5a9
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-26 14:41:14 -0500

    Initial build

--- resources/views/sections/landing/option3/slider.blade.php
+++ resources/views/sections/landing/option3/slider.blade.php
@@ -1,6 +1,7 @@
 <section id="slider" 
 		 class="slider-element slider-parallax full-screen dark slider-parallax-visible" 
-		 style="background: url(https://makstudio.s3.us-east-2.amazonaws.com/backgroundImage2.jpg) center center no-repeat; background-size: cover">
+		 style="height:520px !important"
+		 background: url(https://makstudio.s3.us-east-2.amazonaws.com/backgroundImage2.jpg) center center no-repeat; background-size: cover">
 
 	<div class="slider-parallax-inner">
 
