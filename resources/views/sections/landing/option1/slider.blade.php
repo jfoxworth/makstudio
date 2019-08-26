4ca92ad1109c596eed5bc52ae1b0ebe39dcc91b3
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-26 10:52:41 -0500

    Initial build

--- resources/views/sections/landing/option1/slider.blade.php
+++ resources/views/sections/landing/option1/slider.blade.php
@@ -1,6 +1,6 @@
 <section id="slider" class="slider-element slider-parallax full-screen with-header force-full-screen clearfix">
 
-	<div class="slider-parallax-inner">
+	<div class="slider-inner">
 
 		<div class="full-screen force-full-screen" 
 			 style="background: url('https://makstudio.s3.us-east-2.amazonaws.com/MakWall1.jpg') center center no-repeat; background-size: cover;">
--- resources/views/sections/landing/option2/productVert.blade.php
+++ resources/views/sections/landing/option2/productVert.blade.php
@@ -2,62 +2,283 @@
 
 	<div class="container">
 
-		<div class="row">
-
-			<div class="col-md-8 offset-2 center">
-				<h2 class="text-title-light text-dark mb-5 ls1 uppercase">We design what you dream</h2>
-				<p style="font-size: 17px; color: #777">We design, build, and install custom architectural slat walls, back lit walls, desks, planter walls, faceted walls, wall panels, and more.</p>
-			</div>
-
-			<div class="col-12 mt-5 mb-3">
-
-				<div class="categories-lists justify-content-center clearfix">
-
-					<a href="/products" data-animate="fadeInUp" class="categories" style="background-image: url('https://makstudio.s3.us-east-2.amazonaws.com/slatWallDemo.jpg');">
-						<div class="categories-details">
-							<div class="icon"><i class="icon-settings2"></i></div>
-							<div class="info">Slat Walls</div>
-						</div>
-					</a>
-
-					<a href="/products" data-animate="fadeInUp" data-delay="100" class="categories" style="background-image: url('https://makstudio.s3.us-east-2.amazonaws.com/backlitDemo.jpg');">
-						<div class="categories-details">
-							<div class="icon"><i class="icon-bulb"></i></div>
-							<div class="info">Back Lit Walls</div>
-						</div>
-					</a>
-
-					<a href="/products" data-animate="fadeInUp" data-delay="200" class="categories" style="background-image: url('https://makstudio.s3.us-east-2.amazonaws.com/deskDemo.png');">
-						<div class="categories-details">
-							<div class="icon"><i class="icon-study"></i></div>
-							<div class="info">Desks</div>
-						</div>
-					</a>
-
-					<a href="/products" data-animate="fadeInUp" data-delay="300" class="categories" style="background-image: url('https://makstudio.s3.us-east-2.amazonaws.com/flowerWallDemo.jpg');">
-						<div class="categories-details">
-							<div class="icon"><i class="icon-line-sun"></i></div>
-							<div class="info">Planter Walls</div>
-						</div>
-					</a>
-
-					<a href="/products" data-animate="fadeInUp" data-delay="400" class="categories" style="background-image: url('https://makstudio.s3.us-east-2.amazonaws.com/facetedWallDemo.jpg');">
-						<div class="categories-details">
-							<div class="icon"><i class="icon-realestate-fence"></i></div>
-							<div class="info">Faceted Walls</div>
-						</div>
-					</a>
-
-					<a href="/products" data-animate="fadeInUp" data-delay="500" class="categories" style="background-image: url('https://makstudio.s3.us-east-2.amazonaws.com/wallPanelsDemo.jpg');">
-						<div class="categories-details">
-							<div class="icon"><i class="icon-realestate-bricks"></i></div>
-							<div class="info">Wall Panels</div>
-						</div>
-					</a>
-
-
-				</div>
-			</div>
+		<div class="col-md-8 offset-2 center">
+			<h2 class="text-title-light text-dark mb-5 ls1 uppercase">We design what you dream</h2>
+			<p style="font-size: 17px; color: #777">We design, build, and install custom architectural slat walls, back lit walls, desks, planter walls, faceted walls, wall panels, and more.</p>
 		</div>
+
+
+
+		<div class="row grid-container" data-layout="masonry" style="overflow: visible">
+
+
+
+			<div class="col-lg-4 mb-4">
+
+				<div class="flip-card text-center">
+
+					<div class="flip-card-front dark" 
+						 style="background-image: url('https://makstudio.s3.us-east-2.amazonaws.com/slatWallDemo.jpg')">
+						
+						<div class="flip-card-inner">
+							
+							<div class="card nobg noborder text-center">
+								
+								<div class="card-body">
+									<i class="icon-line2-camera h1"></i>
+									<h3 class="card-title">Slat Walls</h3>
+									<p class="card-text t400">Architectural slat walls add beauty and style to any environment</p>
+								</div>
+							
+							</div>
+						
+						</div>
+					
+					</div>
+					
+					<div class="flip-card-back bg-danger no-after">
+					
+						<div class="flip-card-inner">
+					
+							<p class="mb-2 text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit.</p>
+					
+							<button type="button" class="btn btn-outline-light mt-2">View Details</button>
+					
+						</div>
+					
+					</div>
+				
+				</div>
+			
+			</div>
+		
+
+
+
+
+			<div class="col-lg-4 mb-4">
+
+				<div class="flip-card text-center">
+
+					<div class="flip-card-front dark" 
+						 style="background-image: url('https://makstudio.s3.us-east-2.amazonaws.com/backlitDemo.jpg')">
+						
+						<div class="flip-card-inner">
+							
+							<div class="card nobg noborder text-center">
+								
+								<div class="card-body">
+									<i class="icon-line2-camera h1"></i>
+									<h3 class="card-title">Back Lit Walls</h3>
+									<p class="card-text t400">Back lit walls are a unique style for any modern office</p>
+								</div>
+							
+							</div>
+						
+						</div>
+					
+					</div>
+					
+					<div class="flip-card-back bg-danger no-after">
+					
+						<div class="flip-card-inner">
+					
+							<p class="mb-2 text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit.</p>
+					
+							<button type="button" class="btn btn-outline-light mt-2">View Details</button>
+					
+						</div>
+					
+					</div>
+				
+				</div>
+			
+			</div>
+
+
+
+
+
+
+			<div class="col-lg-4 mb-4">
+
+				<div class="flip-card text-center">
+
+					<div class="flip-card-front dark" 
+						 style="background-image: url('https://makstudio.s3.us-east-2.amazonaws.com/deskDemo.png')">
+						
+						<div class="flip-card-inner">
+							
+							<div class="card nobg noborder text-center">
+								
+								<div class="card-body">
+									<i class="icon-line2-camera h1"></i>
+									<h3 class="card-title">Custom Desks</h3>
+									<p class="card-text t400">Custom Desks for any office environment</p>
+								</div>
+							
+							</div>
+						
+						</div>
+					
+					</div>
+					
+					<div class="flip-card-back bg-danger no-after">
+					
+						<div class="flip-card-inner">
+					
+							<p class="mb-2 text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit.</p>
+					
+							<button type="button" class="btn btn-outline-light mt-2">View Details</button>
+					
+						</div>
+					
+					</div>
+				
+				</div>
+			
+			</div>
+
+
+
+
+
+
+
+			<div class="col-lg-4 mb-4">
+
+				<div class="flip-card text-center">
+
+					<div class="flip-card-front dark" 
+						 style="background-image: url('https://makstudio.s3.us-east-2.amazonaws.com/flowerWallDemo.jpg')">
+						
+						<div class="flip-card-inner">
+							
+							<div class="card nobg noborder text-center">
+								
+								<div class="card-body">
+									<i class="icon-line2-camera h1"></i>
+									<h3 class="card-title">Planter Walls</h3>
+									<p class="card-text t400">Custom Desks for any office environment</p>
+								</div>
+							
+							</div>
+						
+						</div>
+					
+					</div>
+					
+					<div class="flip-card-back bg-danger no-after">
+					
+						<div class="flip-card-inner">
+					
+							<p class="mb-2 text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit.</p>
+					
+							<button type="button" class="btn btn-outline-light mt-2">View Details</button>
+					
+						</div>
+					
+					</div>
+				
+				</div>
+			
+			</div>
+
+
+
+
+
+
+			<div class="col-lg-4 mb-4">
+
+				<div class="flip-card text-center">
+
+					<div class="flip-card-front dark" 
+						 style="background-image: url('https://makstudio.s3.us-east-2.amazonaws.com/facetedWallDemo.jpg')">
+						
+						<div class="flip-card-inner">
+							
+							<div class="card nobg noborder text-center">
+								
+								<div class="card-body">
+									<i class="icon-line2-camera h1"></i>
+									<h3 class="card-title">Faceted Walls</h3>
+									<p class="card-text t400">Custom Desks for any office environment</p>
+								</div>
+							
+							</div>
+						
+						</div>
+					
+					</div>
+					
+					<div class="flip-card-back bg-danger no-after">
+					
+						<div class="flip-card-inner">
+					
+							<p class="mb-2 text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit.</p>
+					
+							<button type="button" class="btn btn-outline-light mt-2">View Details</button>
+					
+						</div>
+					
+					</div>
+				
+				</div>
+			
+			</div>
+
+
+
+
+
+
+
+
+			<div class="col-lg-4 mb-4">
+
+				<div class="flip-card text-center">
+
+					<div class="flip-card-front dark" 
+						 style="background-image: url('https://makstudio.s3.us-east-2.amazonaws.com/wallPanelsDemo.jpg')">
+						
+						<div class="flip-card-inner">
+							
+							<div class="card nobg noborder text-center">
+								
+								<div class="card-body">
+									<i class="icon-line2-camera h1"></i>
+									<h3 class="card-title">Wall Panels</h3>
+									<p class="card-text t400">Custom Desks for any office environment</p>
+								</div>
+							
+							</div>
+						
+						</div>
+					
+					</div>
+					
+					<div class="flip-card-back bg-danger no-after">
+					
+						<div class="flip-card-inner">
+					
+							<p class="mb-2 text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias cum repellat velit.</p>
+					
+							<button type="button" class="btn btn-outline-light mt-2">View Details</button>
+					
+						</div>
+					
+					</div>
+				
+				</div>
+			
+			</div>
+
+
+
+
+		</div>
+
 	</div>
+
 </div>
