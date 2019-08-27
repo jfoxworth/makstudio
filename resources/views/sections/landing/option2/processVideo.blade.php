e7fe0c718c748bd80db27c396204d5e0097325e1
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-26 20:50:23 -0500

    Initial build

--- resources/views/sections/landing/option2/processVideo.blade.php
+++ resources/views/sections/landing/option2/processVideo.blade.php
@@ -10,26 +10,52 @@
 		</div>
 
 
+		<div class="row">
 
-		<div class="col_third" style="width:33% !important;"></div>
+			<div class="col_third" style="width:33% !important;"></div>
 
-		<div class="col_third">
+			<div class="col_third">
 
-			<div style="width:560px;">
+				<div style="width:560px;">
 
-				<iframe width="560" 
-						height="315" 
-						src="https://www.youtube.com/embed/Ir7baoDMz5Y" 
-						frameborder="0" 
-						allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-						
-				</iframe>
+					<iframe width="560" 
+							height="315" 
+							src="https://www.youtube.com/embed/Ir7baoDMz5Y" 
+							frameborder="0" 
+							allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+							
+					</iframe>
+
+				</div>
+
+			</div>
+
+			<div class="col_third col_last" style="width:33% !important;"></div>
+
+		</div>
+
+
+
+		<div class="row center">
+
+			<div class="center"">
+
+				<div style="width:560px;">
+
+					<iframe width="560" 
+							height="315" 
+							src="https://www.youtube.com/embed/Ir7baoDMz5Y" 
+							frameborder="0" 
+							allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+							
+					</iframe>
+
+				</div>
 
 			</div>
 
 		</div>
 
-		<div class="col_third col_last" style="width:33% !important;"></div>
 
 	</div>
 
