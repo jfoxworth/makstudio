201be86115958bff964c45d15e3973901ed09f79
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-26 20:18:57 -0500

    Initial build

--- resources/views/sections/landing/option2/slider.blade.php
+++ resources/views/sections/landing/option2/slider.blade.php
@@ -21,13 +21,17 @@
 		-->
 
 			<div class="divcenter vertical-middle center">
-				<img src="https://makstudio.s3.us-east-2.amazonaws.com/logoBlack.png" width="250px">
-			</div>
 
-			<div class="clear"></div>
+				<div>
+					<img src="https://makstudio.s3.us-east-2.amazonaws.com/logoBlack.png" width="250px">
+				</div>
+				
+				<div class="clear"></div>
 
-			<div class="divcenter center topmargin" style="max-width: 900px;">
-				<h2 class="nobottommargin t300 ls1" style="color:#FFFFFF">Mak studio is a Houston, Texas based group of architects, designers, carpenters, and doers. We model and build custom architectural walls and furniture.</h2>
+				<div class="divcenter center topmargin" style="max-width: 900px;">
+					<h2 class="nobottommargin t300 ls1" style="color:#FFFFFF">Mak studio is a Houston, Texas based group of architects, designers, carpenters, and doers. We model and build custom architectural walls and furniture.</h2>
+				</div>
+
 			</div>
 
 		</div>
