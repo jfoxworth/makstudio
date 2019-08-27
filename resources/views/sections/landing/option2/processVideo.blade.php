144340e1e0b59a80c8e9c9f76b1b1987e7093a69
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-26 20:31:53 -0500

    Initial build

--- resources/views/sections/landing/option2/processVideo.blade.php
+++ resources/views/sections/landing/option2/processVideo.blade.php
@@ -7,13 +7,22 @@
 			<h2 class="text-title-light text-dark mb-5 ls1 uppercase">We handle all steps, from design to installation</h2>
 		</div>
 
-		<iframe width="560" 
-				height="315" 
-				src="https://www.youtube.com/embed/Ir7baoDMz5Y" 
-				frameborder="0" 
-				allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-				
-		</iframe>
+
+		<div class="col_third"></div>
+
+		<div class="col_third">
+
+			<iframe width="560" 
+					height="315" 
+					src="https://www.youtube.com/embed/Ir7baoDMz5Y" 
+					frameborder="0" 
+					allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+					
+			</iframe>
+
+		</div>
+
+		<div class="colr_third col_last"></div>
 
 	</div>
 
