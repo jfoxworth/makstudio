a54f8b1cb76780d75de49bd770c1fd07c8ff3e61
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-26 21:39:47 -0500

    Initial build

--- resources/views/sections/products/splashImage.blade.php
+++ resources/views/sections/products/splashImage.blade.php
@@ -1,6 +1,10 @@
 <!-- Page Title
 ============================================= -->
-<section id="page-title" class="page-title-parallax page-title-dark" style="padding: 250px 0; background-image: url('https://makstudio.s3.us-east-2.amazonaws.com/CNCDemo1.jpg'); background-size: cover; background-position: center center;" data-bottom-top="background-position:0px 400px;" data-top-bottom="background-position:0px -500px;">
+<section id="page-title" 
+		 class="page-title-parallax page-title-dark" 
+		 style="padding: 150px 0; background-image: url('https://makstudio.s3.us-east-2.amazonaws.com/CNCDemo1.jpg'); background-size: cover; background-position: center center;" 
+		 data-bottom-top="background-position:0px 400px;" 
+		 data-top-bottom="background-position:0px -500px;">
 
     <div class="container clearfix">
         <h1>Mak Studio - Products</h1>
