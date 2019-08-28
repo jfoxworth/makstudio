0d9626643257df14c0b1d63d8ba4b544e44d2a38
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-28 10:47:18 -0500

    Initial build

--- resources/views/sections/designStudio/studioBody.blade.php
+++ resources/views/sections/designStudio/studioBody.blade.php
@@ -11,7 +11,7 @@
 
 
 				<!-- Select the type of wall to be built -->
-				<select class="selectpicker" data-style="btn-primary" data-width="19%" style="width:100%; height:35px">
+				<select class="selectpicker" data-style="btn-primary" style="width:100%; height:35px">
 					<option>Architectural Slat Wall</option>
 					<option>Planter Wall</option>
 					<option>Back-lit Branding Wall</option>
@@ -21,12 +21,11 @@
 					</select>
 				</select>
 
-										<select class="selectpicker" data-style="btn-primary" data-width="19%">
+										<select class="selectpicker" data-style="btn-primary" >
 											<option>Mustard</option>
 											<option>Ketchup</option>
 											<option>Relish</option>
 											<option>Onions</option>
-											</select>
 										</select>
 
 
