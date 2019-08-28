586006cdd9d3623ea8c32ad991c881a9c37cb9c6
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-27 20:10:09 -0500

    Initial build

--- resources/views/sections/designStudio/studioBody.blade.php
+++ resources/views/sections/designStudio/studioBody.blade.php
@@ -3,17 +3,29 @@
 
 	<!-- Sidebar
 	============================================= -->
-	<div class="col_onefourth clearfix">
+	<div class="col_one_fourth clearfix">
 		
-		<div class="sidebar-widgets-wrap">
+		<div class="sidebar-widgets-wrap" style="border-right: 1px solid #ccc;"">
 
-			<div class="widget clearfix">
+			<div class="widget clearfix" style="width:80%">
 
-				<select class="selectpicker">
-					<option>Mustard</option>
-					<option>Ketchup</option>
-					<option>Relish</option>
+				<select class="selectpicker" style="width:100%">
+					<option>Architectural Slat Wall</option>
+					<option>Planter Wall</option>
+					<option>Back-lit Branding Wall</option>
+					<option>Reception Desk</option>
+					<option>Faceted Wall</option>
+					<option>Gensler Custom Wall</option>
 				</select>
+
+
+<div class="dropdown bootstrap-select" style="width: 19%;"><select class="selectpicker" data-style="btn-primary" data-width="19%" tabindex="-98">
+<option>Mustard</option>
+<option>Ketchup</option>
+<option>Relish</option>
+<option>Onions</option>
+</select><button type="button" class="btn dropdown-toggle btn-primary" data-toggle="dropdown" role="button" title="Mustard"><div class="filter-option"><div class="filter-option-inner"><div class="filter-option-inner-inner">Mustard</div></div> </div></button><div class="dropdown-menu " role="combobox"><div class="inner show" role="listbox" aria-expanded="false" tabindex="-1"><ul class="dropdown-menu inner show"></ul></div></div></div>
+
 
 			</div>
 
@@ -22,7 +34,7 @@
 	</div>
 
 
-	<div class="col_last col_threefourth">
+	<div class="col_last col_three_fourth">
 		<p>Here</p>
 	</div>
 
