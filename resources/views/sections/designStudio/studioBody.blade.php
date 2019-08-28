6ecfa08bbaf99731c728347005bfe2ee02223dbb
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-28 18:57:31 -0500

    Initial build

--- resources/views/sections/designStudio/studioBody.blade.php
+++ resources/views/sections/designStudio/studioBody.blade.php
@@ -32,6 +32,10 @@
 				</div>
 
 
+<button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="Tooltip on top">
+  Tooltip on top
+</button>
+
 				<div class="divider">
 
 
