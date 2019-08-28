147fbb653978d45499b928de98a5c7c527df9941
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-27 22:32:04 -0500

    Initial build

--- resources/views/sections/designStudio/studioBody.blade.php
+++ resources/views/sections/designStudio/studioBody.blade.php
@@ -3,13 +3,15 @@
 
 	<!-- Sidebar
 	============================================= -->
-	<div class="col_one_fourth clearfix">
+	<div class="col_one_fourth clearfix" style="height:600px; border-right: 1px solid #ccc;">
 		
-		<div class="sidebar-widgets-wrap" style="border-right: 1px solid #ccc;"">
+		<div class="sidebar-widgets-wrap" >
 
 			<div class="widget clearfix" style="width:80%">
 
-				<select class="selectpicker" style="width:100%">
+
+				<!-- Select the type of wall to be built -->
+				<select class="selectpicker btn-primary" style="width:100%; height:35px">
 					<option>Architectural Slat Wall</option>
 					<option>Planter Wall</option>
 					<option>Back-lit Branding Wall</option>
@@ -19,13 +21,126 @@
 				</select>
 
 
-<div class="dropdown bootstrap-select" style="width: 19%;"><select class="selectpicker" data-style="btn-primary" data-width="19%" tabindex="-98">
-<option>Mustard</option>
-<option>Ketchup</option>
-<option>Relish</option>
-<option>Onions</option>
-</select><button type="button" class="btn dropdown-toggle btn-primary" data-toggle="dropdown" role="button" title="Mustard"><div class="filter-option"><div class="filter-option-inner"><div class="filter-option-inner-inner">Mustard</div></div> </div></button><div class="dropdown-menu " role="combobox"><div class="inner show" role="listbox" aria-expanded="false" tabindex="-1"><ul class="dropdown-menu inner show"></ul></div></div></div>
+				<!-- Options for a slat wall -->
+				<select id="slatOptions"
+						class="selectpicker btn-info" 
+						style="width:100%; height:35px">
+					<option>Wall Dimensions</option>
+					<option>Wall Style</option>
+					<option>Fin Options</option>
+					<option>Ripple Style</option>
+					<option>Materials & Color</option>
+					<option>Logo Options</option>
+				</select>
 
+
+				<!-- Options for a backlit wall -->
+				<select id="backlitOptions"
+						class="selectpicker btn-info" 
+						style="width:100%; height:35px">
+					<option>Wall Dimensions</option>
+					<option>Logo</option>
+					<option>Headers</option>
+				</select>
+
+
+
+
+
+				<!-- Specifics for slat wall dimensions -->
+				<div id="slatDimensions">
+
+
+					<div class="white-section">
+						<label>Wall Length (inches)</label>
+						<span class="irs js-irs-0">
+							<span class="irs">
+								<span class="irs-line" tabindex="0">
+									<span class="irs-line-left"></span>
+									<span class="irs-line-mid"></span>
+									<span class="irs-line-right"></span>
+								</span>
+								<span class="irs-min" style="visibility: visible;">48</span>
+								<span class="irs-max" style="visibility: visible;">320</span>
+								<span class="irs-from" style="visibility: hidden;">0</span>
+								<span class="irs-to" style="visibility: hidden;">0</span>
+								<span class="irs-single" style="left: 41.8943%;">50</span>
+							</span>
+							<span class="irs-grid"></span>
+							<span class="irs-bar-edge"></span>
+							<span class="irs-shadow shadow-single" style="display: none;"></span>
+							<span class="irs-slider" style="left: 41.3889%;"></span>
+						</span>
+						<input class="range_01 irs-hidden-input" tabindex="-1" readonly="">
+					</div>
+
+
+
+					<div class="white-section">
+						<label>Wall Height (inches)</label>
+						<span class="irs js-irs-0">
+							<span class="irs">
+								<span class="irs-line" tabindex="0">
+									<span class="irs-line-left"></span>
+									<span class="irs-line-mid"></span>
+									<span class="irs-line-right"></span>
+								</span>
+								<span class="irs-min" style="visibility: visible;">30</span>
+								<span class="irs-max" style="visibility: visible;">540</span>
+								<span class="irs-from" style="visibility: hidden;">0</span>
+								<span class="irs-to" style="visibility: hidden;">0</span>
+								<span class="irs-single" style="left: 41.8943%;">50</span>
+							</span>
+							<span class="irs-grid"></span>
+							<span class="irs-bar-edge"></span>
+							<span class="irs-shadow shadow-single" style="display: none;"></span>
+							<span class="irs-slider" style="left: 41.3889%;"></span>
+						</span>
+						<input class="range_01 irs-hidden-input" tabindex="-1" readonly="">
+					</div>
+
+
+
+
+					<div class="white-section">
+						<label>Wall Depth (inches)</label>
+						<span class="irs js-irs-0">
+							<span class="irs">
+								<span class="irs-line" tabindex="0">
+									<span class="irs-line-left"></span>
+									<span class="irs-line-mid"></span>
+									<span class="irs-line-right"></span>
+								</span>
+								<span class="irs-min" style="visibility: visible;">3</span>
+								<span class="irs-max" style="visibility: visible;">8</span>
+								<span class="irs-from" style="visibility: hidden;">0</span>
+								<span class="irs-to" style="visibility: hidden;">0</span>
+								<span class="irs-single" style="left: 41.8943%;">6</span>
+								<span class="irs-grid" style="width: 93.125%; left: 3.3375%;">
+									<span class="irs-grid-pol" style="left: 0%"></span>
+									<span class="irs-grid-text js-grid-text-0" style="left: 0%; margin-left: -4.96094%;">3</span>
+									<span class="irs-grid-pol" style="left: 20%"></span>
+									<span class="irs-grid-text js-grid-text-1" style="left: 20%; visibility: visible; margin-left: -3.87452%;">4</span>
+									<span class="irs-grid-pol" style="left: 40%"></span>
+									<span class="irs-grid-text js-grid-text-1" style="left: 40%; visibility: visible; margin-left: -3.87452%;">5</span>
+									<span class="irs-grid-pol" style="left: 50%"></span>
+									<span class="irs-grid-text js-grid-text-1" style="left: 60%; visibility: visible; margin-left: -3.87452%;">6</span>
+									<span class="irs-grid-pol" style="left: 80%"></span>
+									<span class="irs-grid-text js-grid-text-1" style="left: 80%; visibility: visible; margin-left: -3.87452%;">7</span>
+									<span class="irs-grid-pol" style="left: 100%"></span>
+									<span class="irs-grid-text js-grid-text-1" style="left: 100%; visibility: visible; margin-left: -3.87452%;">8</span>
+								</span>
+
+							</span>
+							<span class="irs-grid"></span>
+							<span class="irs-bar-edge"></span>
+							<span class="irs-shadow shadow-single" style="display: none;"></span>
+							<span class="irs-slider" style="left: 41.3889%;"></span>
+						</span>
+						<input class="range_01 irs-hidden-input" tabindex="-1" readonly="">
+					</div>
+
+				</div>
 
 			</div>
 
