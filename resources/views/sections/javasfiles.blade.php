6a22f6c0eb28809c03b9f86819006361531d3e58
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-28 11:24:42 -0500

    Initial build

--- resources/views/sections/javasfiles.blade.php
+++ resources/views/sections/javasfiles.blade.php
@@ -2,6 +2,10 @@
     ============================================= -->
     <script type="text/javascript" src="/js/jquery.js"></script>
     <script type="text/javascript" src="/js/plugins.js"></script>
+    <script type="text/javascript" src="/js/rangeslider.min.js"></script>
+    <script type="text/javascript" src="/js/bt-select.js"></script>
+    <script type="text/javascript" src="/js/selectsplitter.js"></script>
+    <script type="text/javascript" src="/js/plugins/jquery.countdown.js"></script>
 
     <!-- Google Map JavaScripts
     ============================================= --
