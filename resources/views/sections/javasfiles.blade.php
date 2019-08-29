362111df20385248d9bfb55a247b58fd3bf8dd1e
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-29 10:49:32 -0500

    Initial build

--- resources/views/sections/javasfiles.blade.php
+++ resources/views/sections/javasfiles.blade.php
@@ -7,7 +7,6 @@
     <script type="text/javascript" src="/js/bs-switches.js"></script>
     <script type="text/javascript" src="/js/bs-select.js"></script>
     <script type="text/javascript" src="/js/selectsplitter.js"></script>
-    <script type="text/javascript" src="/js/function.js"></script>
 
     <!-- Google Map JavaScripts
     ============================================= --
