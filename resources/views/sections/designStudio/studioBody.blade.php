f35896e8d9081650f41d6ea65095ac5c0b0ccfe3
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-29 00:43:45 -0500

    Initial build

--- resources/views/sections/designStudio/studioBody.blade.php
+++ resources/views/sections/designStudio/studioBody.blade.php
@@ -367,6 +367,73 @@
 
 
 
+				<!-- All options for the desks -->
+				<div id="deskSection">
+
+					<div class="white-section" style="margin:20px 0px" >
+
+						<h4 class="center">Select Desk Shape</h4>
+
+						<div>
+							<input id="radio-deskLShape" class="radio-style" name="radio-deskShape" type="radio" checked>
+							<label for="radio-deskLShape" class="radio-style-3-label">L Shape</label>
+						</div>
+						<div>
+							<input id="radio-deskUShape" class="radio-style" name="radio-deskShape" type="radio">
+							<label for="radio-deskUShape" class="radio-style-3-label">U Shape</label>
+						</div>
+						<div>
+							<input id="radio-deskStraight" class="radio-style" name="radio-deskShape" type="radio">
+							<label for="radio-deskStraight" class="radio-style-3-label">Straight</label>
+						</div>
+
+					</div>
+
+
+					<div class="white-section" style="margin:20px 0px" >
+
+						<h4 class="center">Select Desk Color</h4>
+
+						<div>
+							<input id="radio-deskWhite" class="radio-style" name="radio-deskShape" type="radio" checked>
+							<label for="radio-deskWhite" class="radio-style-3-label">White</label>
+						</div>
+						<div>
+							<input id="radio-deskBlack" class="radio-style" name="radio-deskShape" type="radio">
+							<label for="radio-deskBlack" class="radio-style-3-label">Black</label>
+						</div>
+						<div>
+							<input id="radio-deskWood" class="radio-style" name="radio-deskShape" type="radio">
+							<label for="radio-deskWood" class="radio-style-3-label">Wood Tone</label>
+						</div>
+
+					</div>
+
+
+
+					<div class="white-section" style="margin:20px 0px" >
+
+						<h4 class="center">Desk Size (People)</h4>
+
+						<div>
+							<input id="radio-deskPeople1" class="radio-style" name="radio-deskShape" type="radio" checked>
+							<label for="radio-deskPeople1" class="radio-style-3-label">1</label>
+						</div>
+						<div>
+							<input id="radio-deskPeople2" class="radio-style" name="radio-deskShape" type="radio">
+							<label for="radio-deskPeople2" class="radio-style-3-label">2</label>
+						</div>
+						<div>
+							<input id="radio-deskPeople3" class="radio-style" name="radio-deskShape" type="radio">
+							<label for="radio-deskPeople3" class="radio-style-3-label">3</label>
+						</div>
+
+					</div>
+
+
+					<div class="divcenter">
+						<a href="#" class="button button-border button-rounded button-fill button-blue"><span>Submit</span></a>
+					</div>
 
 
 				</div>
