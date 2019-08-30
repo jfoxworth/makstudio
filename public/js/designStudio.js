137512959f474e7d348d90c7df7756994c780979
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-08-29 22:05:46 -0500

    Initial build

--- public/js/designStudio.js
+++ public/js/designStudio.js
@@ -8,17 +8,11 @@ $(document).ready(function()	{
 	var  _viewerSettings = { 
 		// container to use 
 		container: _container, 
-		// when creating the viewer, we want to get back an API v2 object 
-		api: {version: 2}, 
-		// level of log messages which will be sent to the browser console
-		loggingLevel: SDVApp.constants.loggingLevels.NONE, 
-		// instantly show the 3D scene 
-		showSceneMode: SDVApp.constants.showSceneModes.INSTANT, 
 		// ticket for a ShapeDiver model 
 		ticket: '85d9977f5426033e058a60c8b81c2c91d331d5eec7bdfed66d354132539d4efb0d01bd6e5d7bc61bc8894b820fff1922d2a2e7e6b7f5883d244f93f0c9ce73daa540ccd7b6d1a1d9ccc40307c3549ff3bbce1a7e902ac438abb53b472d73f2fd2578881f8846e73c4af238f34b9725c41232a2e3129d-d34ace0dbe577c6152ee63654b9e807c', 
 		modelViewUrl : 'eu-central-1',
-		width:'700px',
-		height:'500px'
+		brandedMode: false,
+		deferGeometryLoading: waitParams	
 	}; 
 
 	// create the viewer, get back an API v2 object 
