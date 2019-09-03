7808046e99a5c996362f274414d4076e7ad66657
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-09-03 16:34:01 -0500

    back end build

--- resources/views/sections/head.blade.php
+++ resources/views/sections/head.blade.php
@@ -5,6 +5,13 @@
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-89103879-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-89103879-1');
+    </script>
 
     <script>
       window.dataLayer = window.dataLayer || [];
