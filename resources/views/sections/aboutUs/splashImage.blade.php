2471386da197d98782872fa1052006ec85b96e20
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-09-04 11:05:18 -0500

    back end build

--- resources/views/sections/aboutUs/splashImage.blade.php
+++ resources/views/sections/aboutUs/splashImage.blade.php
@@ -3,7 +3,7 @@
 <section id="page-title" class="page-title-parallax page-title-dark" style="padding: 150px 0; background-image: url('https://makstudio.s3.us-east-2.amazonaws.com/fullStaffDark.jpg'); background-size: cover; background-position: center center;" data-bottom-top="background-position:0px 400px;" data-top-bottom="background-position:0px -500px;">
 
     <div class="container clearfix">
-        <h1>Mak Studio - Who we are</h1>
+        <h1 style="text-transform:none">M<div style="text-decoration:overline; display:inline">a</div>k Studio - Who we are</h1>
         <span>Everything you need to know about our Company</span>
     </div>
 
