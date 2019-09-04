e6e7bdc15e1b6d07b5e949a09681febd2c67f756
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-09-04 14:37:29 -0500

    back end build

--- public/js/designStudio.js
+++ public/js/designStudio.js
@@ -690,12 +690,12 @@ function retrieveModels(  )
 
         $("#userModelList").html('');
 
-		var tr="<tr style='padding:10px 0px;'><th style='width:300px;'>Model Name</th><th style='width:300px;'>Date Created</th></tr>"
+		var tr="<tr style='padding:10px 0px; border-bottom:2px solid #ccc;'><th style='width:300px;'>Model Name</th><th style='width:300px;'>Date Created</th></tr>"
         $("#userModelList").append(tr);
 
 		$.each(userModelData, function(index, obj){
 	        
-	        var tr = $("<tr></tr>");
+	        var tr = $("<tr class='hoverMe' style='padding:10px 0px; border-bottom:2px solid #ccc;'></tr>");
 	        tr.append("<td class='potenModel hoverMe' id='"+obj.id+"'>"+ obj.build_data.name +"</td>");
 	        tr.append("<td class='potenModel hoverMe' id='"+obj.id+"'>"+ obj.created_at +"</td>");
 	        tr.append("<td id='"+obj.id+"' class='deleteModel hoverMe'><i id='"+obj.id+"' class='icon-remove'></i></td>");
