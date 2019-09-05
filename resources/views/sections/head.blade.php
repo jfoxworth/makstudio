8d559b05f1ac762ef8ee949ff63a680f05546cc5
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-09-05 15:45:17 -0500

    back end build

--- resources/views/sections/head.blade.php
+++ resources/views/sections/head.blade.php
@@ -28,7 +28,7 @@
     ============================================= -->
     <link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="/css/bootstrap.css" type="text/css" />
-    <link rel="stylesheet" href="/css/style2.css" type="text/css" />
+    <link rel="stylesheet" href="/css/style.css" type="text/css" />
     <link rel="stylesheet" href="/css/dark.css" type="text/css" />
     <link rel="stylesheet" href="/css/font-icons.css" type="text/css" />
     <link rel="stylesheet" href="/css/font-icons/real-estate/real-estate-icons.css" type="text/css" />
