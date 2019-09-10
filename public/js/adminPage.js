0ffa92208ecef529b65283dd0444a54b9d91ffa7
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-09-10 12:55:48 -0500

    fix bugs

--- public/js/adminPage.js
+++ public/js/adminPage.js
@@ -9,9 +9,9 @@ $(document).ready(function()	{
 
 	// Set CSRF Token
 	$.ajaxSetup({
-	    headers: {
-	        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-	    }
+		headers: {
+			'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+		}
 	});
 
 
@@ -69,32 +69,32 @@ function retrieveMessages( )
 		$.each(data, function(index, obj){
 
 			obj.message_data = JSON.parse(obj.message_data);
-	        
-	        var message = '';
+			
+			var message = '<div class="row">';
 
-	        message=message+"<div style='width:600px; margin-top:20px;' class='row'><div style='width:150px; font-weight:bold;'>Name : </div>";
-	        message=message+"<div style='width:450px'>"+ obj.message_data.name +"</div></div>";
+			message=message+"<div class='col_one_third'><div style='font-weight:bold;'>Name : </div>";
+			message=message+"<div class='col_two_third col_last'>"+ obj.message_data.name +"</div></div>";
 
-	        message=message+"<div style='width:600px; margin-top:20px;' class='row'><div style='width:150px; font-weight:bold;'>Date Sent : </div>";
-	        message=message+"<div style='width:450px'>"+ obj.created_at +"</div></div>";
+			message=message+"<div class='col_one_third'><div style='font-weight:bold;'>Name : </div>";
+			message=message+"<div class='col_two_third col_last'>"+ obj.created_at +"</div></div>";
 
-	        message=message+"<div style='width:600px; margin-top:20px;' class='row'><div style='width:150px; font-weight:bold;'>Phone Number : </div>";
-	        message=message+"<div style='width:450px'>"+ obj.message_data.phone +"</div></div>";
+			message=message+"<div class='col_one_third'><div style='font-weight:bold;'>Name : </div>";
+			message=message+"<div class='col_two_third col_last'>"+ obj.message_data.phone +"</div></div>";
 
-	        message=message+"<div style='width:600px; margin-top:20px;' class='row'><div style='width:150px; font-weight:bold;'>email : </div>";
-	        message=message+"<div style='width:450px'>"+ obj.message_data.email +"</div></div>";
+			message=message+"<div class='col_one_third'><div style='font-weight:bold;'>Name : </div>";
+			message=message+"<div class='col_two_third col_last'>"+ obj.message_data.email +"</div></div>";
 
-	        message=message+"<div style='width:600px; margin-top:20px;' class='row'><div style='width:150px; font-weight:bold;'>Subject : </div>";
-	        message=message+"<div style='width:450px'>"+ obj.message_data.subject +"</div></div>";
+			message=message+"<div class='col_one_third'><div style='font-weight:bold;'>Name : </div>";
+			message=message+"<div class='col_two_third col_last'>"+ obj.message_data.subject +"</div></div>";
 
-	        message=message+"<div style='width:600px; margin-top:40px; margin-bottom:60px; border-bottom:1px solid #ccc'><td colspan='2' style='width:600px;'>"+obj.message_data.message+"</div></div>";
-
-
-	        console.log('The message is '+message);
+			message=message+"<div class='col_full' style='border-bottom:1px solid #ccc'>"+obj.message_data.message+"</div>";
 
 
-	        $("#messageList").append(message);
-	    });
+			message = message+'</div>';
+
+
+			$("#messageList").append(message);
+		});
 
 	});
 
@@ -120,6 +120,32 @@ function retrieveBuilds( )
 	{
 		console.log('Builds are ...');
 		console.log(data);
+
+
+		$.each(data, function(index, obj){
+
+			obj.build_data = JSON.parse(obj.build_data);
+			
+			var builds = '<div class="row">';
+
+			builds=builds+"<div class='col_one_third'><div style='font-weight:bold;'>Build Type : </div>";
+			builds=builds+"<div class='col_two_third col_last'>"+ obj.build_id +"</div></div>";
+
+			builds=builds+"<div class='col_one_third'><div style='font-weight:bold;'>Name : </div>";
+			builds=message+"<div class='col_two_third col_last'>"+ obj.created_at +"</div></div>";
+
+			builds=message+"<div class='col_one_third'><div style='font-weight:bold;'>Name : </div>";
+			builds=message+"<div class='col_two_third col_last'>"+ obj.build_data.name +"</div></div>";
+
+
+
+			builds = builds+'</div>';
+
+
+			$("#buildList").append(builds);
+		});
+
+
 
 	});
 
