34c2a6db943e7683e2047c47275387d5a6defedb
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-09-10 14:29:15 -0500

    fix bugs

--- public/js/adminPage.js
+++ public/js/adminPage.js
@@ -72,19 +72,19 @@ function retrieveMessages( )
 			
 			var message = '';
 
-			message=message+"<div class='row'><div class='col_one_third'><div style='font-weight:bold;'>Name : </div>";
+			message=message+"<div class='row'><div class='col_one_third' style='font-weight:bold;'>Name : </div>";
 			message=message+"<div class='col_two_third col_last'>"+ obj.message_data.name +"</div></div>";
 
-			message=message+"<div class='row'><div class='col_one_third'><div style='font-weight:bold;'>Name : </div>";
+			message=message+"<div class='row'><div class='col_one_third' style='font-weight:bold;'>Name : </div>";
 			message=message+"<div class='col_two_third col_last'>"+ obj.created_at +"</div></div>";
 
-			message=message+"<div class='row'><div class='col_one_third'><div style='font-weight:bold;'>Name : </div>";
+			message=message+"<div class='row'><div class='col_one_third' style='font-weight:bold;'>Name : </div>";
 			message=message+"<div class='col_two_third col_last'>"+ obj.message_data.phone +"</div></div>";
 
-			message=message+"<div class='row'><div class='col_one_third'><div style='font-weight:bold;'>Name : </div>";
+			message=message+"<div class='row'><div class='col_one_third' style='font-weight:bold;'>Name : </div>";
 			message=message+"<div class='col_two_third col_last'>"+ obj.message_data.email +"</div></div>";
 
-			message=message+"<div class='row'><div class='col_one_third'><div style='font-weight:bold;'>Name : </div>";
+			message=message+"<div class='row'><div class='col_one_third' style='font-weight:bold;'>Name : </div>";
 			message=message+"<div class='col_two_third col_last'>"+ obj.message_data.subject +"</div></div>";
 
 			message=message+"<div class='col_full' style='border-bottom:1px solid #ccc'>"+obj.message_data.message+"</div></div>";
@@ -125,13 +125,13 @@ function retrieveBuilds( )
 			
 			var builds = '';
 
-			builds=builds+"<div class='row'><div class='col_one_third'><div style='font-weight:bold;'>Build Type : </div>";
+			builds=builds+"<div class='row'><div class='col_one_third' style='font-weight:bold;'>Build Type : </div>";
 			builds=builds+"<div class='col_two_third col_last'>"+ obj.build_id +"</div></div>";
 
-			builds=builds+"<div class='row'><div class='col_one_third'><div style='font-weight:bold;'>Name : </div>";
+			builds=builds+"<div class='row'><div class='col_one_third' style='font-weight:bold;'>Name : </div>";
 			builds=builds+"<div class='col_two_third col_last'>"+ obj.created_at +"</div></div>";
 
-			builds=builds+"<div class='row'><div class='col_one_third'><div style='font-weight:bold;'>Name : </div>";
+			builds=builds+"<div class='row'><div class='col_one_third' style='font-weight:bold;'>Name : </div>";
 			builds=builds+"<div class='col_two_third col_last'>"+ obj.build_data.name +"</div></div>";
 
 
