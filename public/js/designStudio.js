bf0618e8df6e1d61a28249702d0c37ffa7e2e1bf
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-09-11 09:40:30 -0500

    additions to studio

--- public/js/designStudio.js
+++ public/js/designStudio.js
@@ -773,9 +773,9 @@ function retrieveModels(  )
 			tr.append("<td class='potenModel hoverMe' id='"+obj.id+"'>"+ obj.build_data.name +"</td>");
 			tr.append("<td class='potenModel hoverMe' id='"+obj.id+"'>"+ formattedDate +"</td>");
 			tr.append("<td id='"+obj.id+"' class='deleteModel hoverMe' style='padding:0px 10px 0px 0px;'>");
-			tr.append("<i id='"+obj.id+"' class='icon-list2' data-toggle="tooltip" data-placement="top" title="Model Details"></i>");
-			tr.append("<i id='"+obj.id+"' class='icon-resize-full' data-toggle="tooltip" data-placement="top" title="Open Model"></i>");
-			tr.append("<i id='"+obj.id+"' class='icon-remove-circle' data-toggle="tooltip" data-placement="top" title="Delete Model"></i>");
+			tr.append("<i id='"+obj.id+"' class='icon-list2' data-toggle='tooltip' data-placement='top' title='Model Details'></i>");
+			tr.append("<i id='"+obj.id+"' class='icon-resize-full' data-toggle='tooltip' data-placement='top' title='Open Model'></i>");
+			tr.append("<i id='"+obj.id+"' class='icon-remove-circle' data-toggle='tooltip' data-placement='top' title='Delete Model'></i>");
 			tr.append("</td></tr>");
 
 			$("#userModelList").append(tr);
