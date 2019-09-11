087e97363801533390c350ff59fd7c2055cb2f21
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-09-11 09:53:47 -0500

    additions to studio

--- public/js/designStudio.js
+++ public/js/designStudio.js
@@ -762,7 +762,7 @@ function retrieveModels(  )
 		$("#userModelList").html('');
 
 		var tr="<tr style='padding:10px 0px; border-bottom:2px solid #ccc;'>";
-		tr=tr+"<th style='width:300px;'>Model Name</th><th style='width:300px;'>Date Created</th><th colspan='3'>Actions</th>";
+		tr=tr+"<th style='width:250px;'>Model Name</th><th style='width:250px;'>Date Created</th><th style='width:100px;' colspan='3'>Actions</th>";
 		tr=tr+"</tr>"
 		$("#userModelList").append(tr);
 
@@ -771,16 +771,16 @@ function retrieveModels(  )
 			var newDate = new Date(obj.created_at);
 			var formattedDate = monthNames[newDate.getMonth()] +' '+newDate.getDay()+','+newDate.getFullYear();
 
-			var tr = $("<tr class='hoverMeGrey' style='height:50px; padding:10px 0px 10px 10px; border-bottom:2px solid #ccc;'>");
-			tr.append("<td class='potenModel hoverMe' id='"+obj.id+"'>"+ obj.build_data.name +"</td>");
-			tr.append("<td class='potenModel hoverMe' id='"+obj.id+"'>"+ formattedDate +"</td>");
-			tr.append("<td id='"+obj.id+"' class='modelDetails hoverMe' style='padding:0px 10px 0px 0px;'>");
+			var tr = $("<tr style='height:50px; padding:10px 0px 10px 10px; border-bottom:2px solid #ccc;'>");
+			tr.append("<td>"+ obj.build_data.name +"</td>");
+			tr.append("<td>"+ formattedDate +"</td>");
+			tr.append("<td id='"+obj.id+"' class='modelDetails hoverMe' style='padding:10px 10px 0px 0px;'>");
 			tr.append("<i id='"+obj.id+"' class='icon-list2 h5' data-toggle='tooltip' data-placement='top' title='Model Details'></i>");
 			tr.append("</td>");
-			tr.append("<td id='"+obj.id+"' class='openModel hoverMe' style='padding:0px 10px 0px 10px;'>");
+			tr.append("<td id='"+obj.id+"' class='potenModel hoverMe' style='padding:10px 10px 0px 10px;'>");
 			tr.append("<i id='"+obj.id+"' class='icon-resize-full h5' data-toggle='tooltip' data-placement='top' title='Open Model'></i>");
 			tr.append("</td>");
-			tr.append("<td id='"+obj.id+"' class='deleteModel hoverMe' style='padding:0px 10px 0px 10px;'>");
+			tr.append("<td id='"+obj.id+"' class='deleteModel hoverMe' style='padding:10px 10px 0px 10px;'>");
 			tr.append("<i id='"+obj.id+"' class='icon-remove-circle h5' data-toggle='tooltip' data-placement='top' title='Delete Model'></i>");
 			tr.append("</td></tr>");
 
@@ -791,7 +791,6 @@ function retrieveModels(  )
 
 
 }
-
 
 
 
