8ee17e4da99c7fecbd9a3cc120480bc2bb64de25
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-09-10 23:44:33 -0500

    additions to studio

--- resources/views/sections/designStudio/areaDisplays/desk.blade.php
+++ resources/views/sections/designStudio/areaDisplays/desk.blade.php
@@ -1,3 +1,88 @@
-		<div id="deskDisplay" style="width:900px; height:600px;" class="center">
-			<img src="https://makstudio.s3.us-east-2.amazonaws.com/deskDemo.png" width="750px">
+<div id="deskDisplay" style="width:900px; height:600px;" class="center">
+	<img src="https://makstudio.s3.us-east-2.amazonaws.com/deskDemo.png" width="750px">
+</div>
+
+
+<div class="center">
+
+	<div class="white-section center" style="margin:20px 0px" >
+
+		<h4 class="center nobottommargin">Select Desk Shape</h4>
+
+		<div>
+			<input id="radio-deskLShape" class="radio-style" name="radio-deskShape" type="radio" checked>
+			<label for="radio-deskLShape" class="radio-style-3-label">L Shape</label>
 		</div>
+		<div>
+			<input id="radio-deskUShape" class="radio-style" name="radio-deskShape" type="radio">
+			<label for="radio-deskUShape" class="radio-style-3-label">U Shape</label>
+		</div>
+		<div>
+			<input id="radio-deskStraight" class="radio-style" name="radio-deskShape" type="radio">
+			<label for="radio-deskStraight" class="radio-style-3-label">Straight</label>
+		</div>
+
+	</div>
+
+</div>
+
+
+
+<div class="center">
+
+	<div class="white-section center" style="margin:20px 0px" >
+
+		<h4 class="center nobottommargin">Select Desk Color</h4>
+
+		<div>
+			<input id="radio-deskWhite" class="radio-style" name="radio-deskColor" type="radio" checked>
+			<label for="radio-deskWhite" class="radio-style-3-label">White</label>
+		</div>
+		<div>
+			<input id="radio-deskBlack" class="radio-style" name="radio-deskColor" type="radio">
+			<label for="radio-deskBlack" class="radio-style-3-label">Black</label>
+		</div>
+		<div>
+			<input id="radio-deskWood" class="radio-style" name="radio-deskColor" type="radio">
+			<label for="radio-deskWood" class="radio-style-3-label">Wood Tone</label>
+		</div>
+
+	</div>
+
+</div>
+
+
+
+<div class="center">
+
+
+	<div class="white-section center" style="margin:20px 0px" >
+
+		<h4 class="center nobottommargin">Desk Size (People)</h4>
+
+		<div>
+			<input id="radio-deskPeople1" class="radio-style" name="radio-deskSize" type="radio" checked>
+			<label for="radio-deskPeople1" class="radio-style-3-label">1</label>
+		</div>
+		<div>
+			<input id="radio-deskPeople2" class="radio-style" name="radio-deskSize" type="radio">
+			<label for="radio-deskPeople2" class="radio-style-3-label">2</label>
+		</div>
+		<div>
+			<input id="radio-deskPeople3" class="radio-style" name="radio-deskSize" type="radio">
+			<label for="radio-deskPeople3" class="radio-style-3-label">3</label>
+		</div>
+
+	</div>
+
+</div>
+
+
+
+<div class="center">
+
+	<div class="divcenter">
+		<a href="#" class="button button-border button-rounded button-fill button-blue"><span>Submit</span></a>
+	</div>
+
+</div>
--- resources/views/sections/designStudio/areaDisplays/planterWall.blade.php
+++ resources/views/sections/designStudio/areaDisplays/planterWall.blade.php
@@ -1,3 +1,91 @@
-		<div id="planterWallDisplay" style="width:900px; height:600px;">
-			<img src="https://makstudio.s3.us-east-2.amazonaws.com/planterWall.jpg" width="750px">
+<div id="planterWallDisplay" style="width:900px; height:600px;">
+	<img src="https://makstudio.s3.us-east-2.amazonaws.com/planterWall.jpg" width="750px">
+</div>
+
+
+<div class="center">
+
+	<div class="white-section center row" style="margin:20px 0px" >
+
+		<h4 class="center nobottommargin">Choose Fin Style</h4>
+
+		<div>
+			<input id="radio-planterFinTwisted" class="radio-style" name="radio-planterFinStyle" type="radio" checked>
+			<label for="radio-planterFinTwisted" class="radio-style-3-label">Twisted</label>
 		</div>
+		<div>
+			<input id="radio-planterFinLogo" class="radio-style" name="radio-planterFinStyle" type="radio">
+			<label for="radio-planterFinLogo" class="radio-style-3-label">Logo</label>
+		</div>
+		<div>
+			<input id="radio-planterFinStraight" class="radio-style" name="radio-planterFinStyle" type="radio">
+			<label for="radio-planterFinStraight" class="radio-style-3-label">Straight</label>
+		</div>
+
+	</div>
+
+</div>
+
+
+
+
+<div class="center">
+
+	<div class="white-section center row" style="margin:20px 0px" >
+
+		<h4 class="center nobottommargin">Planter Box Color</h4>
+
+		<div>
+			<input id="radio-planterWhite" class="radio-style" name="radio-planterColor" type="radio" checked>
+			<label for="radio-planterWhite" class="radio-style-3-label">White</label>
+		</div>
+		<div>
+			<input id="radio-planterBlack" class="radio-style" name="radio-planterColor" type="radio">
+			<label for="radio-planterBlack" class="radio-style-3-label">Black</label>
+		</div>
+		<div>
+			<input id="radio-planterBlack" class="radio-style" name="radio-planterColor" type="radio">
+			<label for="radio-planterBlack" class="radio-style-3-label">Blue</label>
+		</div>
+		<div>
+			<input id="radio-planterWood" class="radio-style" name="radio-planterColor" type="radio">
+			<label for="radio-planterWood" class="radio-style-3-label">Wood Tone</label>
+		</div>
+		<div>
+			<input id="radio-planterWood" class="radio-style" name="radio-planterColor" type="radio">
+			<label for="radio-planterWood" class="radio-style-3-label">Other</label>
+		</div>
+
+	</div>
+
+</div>
+
+
+<div class="center">
+
+
+	<div class="white-section center row" style="margin:20px 0px" >
+
+		<h4 class="center nobottommargin">Number Needed</h4>
+
+		<div>
+			<input id="radio-planterNumber1" class="radio-style" name="radio-planterNumber" type="radio" checked>
+			<label for="radio-planterNumber1" class="radio-style-3-label">1</label>
+		</div>
+		<div>
+			<input id="radio-planterNumber2" class="radio-style" name="radio-planterNumber" type="radio">
+			<label for="radio-planterNumber2" class="radio-style-3-label">2</label>
+		</div>
+		<div>
+			<input id="radio-planterNumber3" class="radio-style" name="radio-planterNumber" type="radio">
+			<label for="radio-planterNumber3" class="radio-style-3-label">3</label>
+		</div>
+
+	</div>
+
+
+	<div class="divcenter">
+		<a href="#" class="button button-border button-rounded button-fill button-blue"><span>Submit</span></a>
+	</div>
+
+</div>
