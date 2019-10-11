f52e31b3485de05d6eea42fa87a31ce47bf5b3ff
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-10-11 13:03:44 -0500

    fixing bugs

--- resources/views/sections/landing/paraSlider.blade.php
+++ resources/views/sections/landing/paraSlider.blade.php
@@ -50,7 +50,8 @@
 					data-elementdelay="0.01"
 					data-endelementdelay="0.1"
 					data-endspeed="1000"
-					data-endeasing="Power4.easeIn" style="z-index: 3;font-size:40px">Customize Your Interior Space</div>
+					data-endeasing="Power4.easeIn" style="text-align:center; z-index: 3; font-size:40px; width:450px !important; background-color:#ddd; color:#000; padding:10px">
+					Customize Your Interior Space</div>
 
 
 					<!--
@@ -84,8 +85,8 @@
 					data-endelementdelay="0.1"
 					data-endspeed="1000"
 					data-endeasing="Power4.easeIn" style="z-index: 3; color:#222;">
-					<a href="/designStudio" class="button button-border button-white button-light button-large button-rounded tright nomargin">
-						<span>Create Your Own</span> <i class="icon-angle-right"></i>
+					<a href="/designStudio" class="button button-3d button-white button-large button-rounded tright nomargin" style="color: #000;">
+						<span>Create your own</span> <i class="icon-angle-right"></i>
 					</a>
 					</div>
 				</li>
@@ -326,7 +327,7 @@
 					data-elementdelay="0.01"
 					data-endelementdelay="0.1"
 					data-endspeed="1000"
-					data-endeasing="Power4.easeIn" style="text-align:center; z-index: 3; font-size:40px; max-width:450px !important; background-color:#222; padding:10px">Memorable experiences</div>
+					data-endeasing="Power4.easeIn" style="text-align:center; z-index: 3; font-size:40px; width:450px !important; background-color:#222; padding:10px">Memorable experiences</div>
 
 					<div class="tp-caption ltl tp-resizeme revo-slider-emphasis-text noborder"
 					data-x="0"
