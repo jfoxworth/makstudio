66e5a55cfbf4ef4943242077ab65ee8258703332
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-10-14 22:10:04 -0500

    adding inputs

--- resources/views/sections/designStudio/studioBody.blade.php
+++ resources/views/sections/designStudio/studioBody.blade.php
@@ -489,7 +489,7 @@
 						<div class="white-section center" style="margin:20px 0px" id="finColor">
 							<label>Color</label>
 							<div class="row" style="width:100%">
-								<input type="text" class="form-control" id="finWallColor" value="#A6FF00" data-color-format="hex">
+								<div class="pickr"></div>
 							</div>
 						</div>
 
