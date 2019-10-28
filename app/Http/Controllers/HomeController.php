c34c44b63ad24addbd72c80d2160c53cc4446c80
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-10-28 10:50:21 -0500

    amplitude add ins

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends Controller
     public function index()
     {
 		//        return view('home');
-        $instanceData = Instance::where( 'user_id', '=', Auth::id())->orderBy('created_at')->get() );
+        $instanceData = Instance::where( 'user_id', '=', Auth::id())->orderBy('created_at')->get();
         return view('home')->with('instanceData', $instanceData);
     }
 }
