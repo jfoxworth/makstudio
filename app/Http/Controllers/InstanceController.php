71820894ae34869e295ded3016a21e596679c76c
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-11-06 02:03:29 -0600

    Admin Background

--- app/Http/Controllers/InstanceController.php
+++ app/Http/Controllers/InstanceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Instance;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Events\NewInstance;
 
 
 class InstanceController extends Controller
