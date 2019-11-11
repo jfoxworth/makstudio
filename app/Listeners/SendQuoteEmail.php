a693c0d8ca574fb94fa72c157d272131d7c487f7
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-11-10 19:26:41 -0600

    Add Email

--- app/Listeners/SendQuoteEmail.php
+++ app/Listeners/SendQuoteEmail.php
@@ -102,7 +102,7 @@ class SendQuoteEmail
 		$from = new SendGrid\Email(null, "test@example.com");
 		$subject = "Hello World from the SendGrid PHP Library!";
 		$to = new SendGrid\Email(null, "jfoxworth@cadwolf.com");
-		$content = new SendGrid\Content("text/plain", "Hello, Email!");
+		$content = new SendGrid\Content("text/plain", $emailText);
 		$mail = new SendGrid\Mail($from, $subject, $to, $content);
 
 		$apiKey = getenv('SENDGRID_API_KEY');
@@ -113,6 +113,20 @@ class SendQuoteEmail
 		echo $response->headers();
 		echo $response->body();
 
+
+		$from = new SendGrid\Email(null, "test@example.com");
+		$subject = "Hello World from the SendGrid PHP Library!";
+		$to = new SendGrid\Email(null, "jfoxworth@cadwolf.com");
+		$content = new SendGrid\Content("text/html", $emailText);
+		$mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+		$apiKey = getenv('SENDGRID_API_KEY');
+		$sg = new \SendGrid($apiKey);
+
+		$response = $sg->client->mail()->send()->post($mail);
+		echo $response->statusCode();
+		echo $response->headers();
+		echo $response->body();
 
 
     }
