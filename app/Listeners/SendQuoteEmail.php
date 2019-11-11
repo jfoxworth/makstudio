42e6009bd8b38fa46e83b0827b2fbcc5d54211b1
Joshua Foxworth <jfoxworth@cadwolf.com>
2019-11-10 19:00:46 -0600

    Add Email

--- app/Listeners/SendQuoteEmail.php
+++ app/Listeners/SendQuoteEmail.php
@@ -33,38 +33,39 @@ class SendQuoteEmail
     	if ( $event->build['build_id'] == "fossil" )
     	{
 
-				$emailText = '<table><tr>
-						<th>Quantity</th>
+				$emailText = '<table class="modalTable center"
+					   style="width:500px; margin-left:25px">
+
+					<tr style="height:35px; border-bottom:1px solid #ccc; font-weight:bold">
+						<th style="width:300px">Quantity</th>
 						<th>Amount</th>
 					</tr>
 
-					<tr>
-						<td>Width of Wall</td><td>'.$event->build['build_data']['componentValues']['Wall Width'].'</td>
+					<tr style="height:30px; border-bottom:1px solid #ccc">
+						<td>Width of Wall</td><td id="fossilWidthQuote">'.$event->build['build_data']['componentValues']['Wall Width'].'</td>
 					</tr>
 
-					<tr>
-						<td>Height of Wall</td><td>'.$event->build['build_data']['componentValues']['Wall Height'].'</td>
+					<tr style="height:30px; border-bottom:1px solid #ccc">
+						<td>Height of Wall</td><td id="fossilHeightQuote">'.$event->build['build_data']['componentValues']['Wall Height'].'</td>
 					</tr>
 
-					<tr>
-						<td>Fossil Shape Count</td><td>'.$event->build['build_data']['componentValues']['Shape Count'].'</td>
+					<tr style="height:30px; border-bottom:1px solid #ccc">
+						<td>Fossil Shape Count</td><td id="fossilShapeCountQuote">'.$event->build['build_data']['componentValues']['Shape Count'].'</td>
 					</tr>
 
-					<tr>
-						<td>Fossil Factor</td><td>'.$event->build['build_data']['componentValues']['Factor'].'</td>
+					<tr style="height:30px; border-bottom:1px solid #ccc">
+						<td>Fossil Factor</td><td id="fossilFactorQuote">'.$event->build['build_data']['componentValues']['Factor'].'</td>
 					</tr>
 
-					<tr>
-						<td>Price</td><td>'.$event->build['build_data']['price'].'</td>
+					<tr style="height:30px; background-color:#888; color:#fff">
+						<td>Price</td><td id="fossilModalPriceQuote">'.$event->build['build_data']['price'].'</td>
 					</tr>
 
 				</table>';
 
-				$emailText = 'Wall Width - '.$event->build['build_data']['componentValues']['Wall Width'];
 
     	}
 
-    	echo($emailText);
 
 		$request_body = json_decode('{
 		  "personalizations": [
