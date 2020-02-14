48e0b8d1bb910d094a618fa641892bd30f484f86
Joshua Foxworth <jfoxworth@cadwolf.com>
2020-02-14 09:19:08 -0600

    coming soon

---
+++ resources/views/sections/headerLight.blade.php
@@ -0,0 +1,34 @@
+<!-- Header
+============================================= -->
+<header id="header" class="full-header border-full-header transparent-header static-sticky" data-sticky-class="not-dark">
+
+    <div id="header-wrap">
+
+        <div class="container clearfix">
+
+            <div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+
+            <!-- Logo
+            ============================================= -->
+            <div id="logo">
+
+                <a href="/" class="standard-logo" data-dark-logo="https://makstudio.s3.us-east-2.amazonaws.com/makLogoWhiteSmall.png">
+                    <img src="https://makstudio.s3.us-east-2.amazonaws.com/makLogoBlackSmall.png" alt="Mak Studio" >
+                </a>
+                
+                <a href="/" class="retina-logo" data-dark-logo="https://makstudio.s3.us-east-2.amazonaws.com/makLogoWhiteSmall.png">
+                    <img src="https://makstudio.s3.us-east-2.amazonaws.com/makLogoBlackSmall.png" alt="Mak Studio" >
+                </a>
+   
+            </div><!-- #logo end -->
+
+
+
+            @include('sections.headermenu')
+
+
+        </div>
+
+    </div>
+
+</header><!-- #header end -->
