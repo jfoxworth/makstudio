955c7e1d593a5dc786a05e33a38005e8db221895
Joshua Foxworth <jfoxworth@cadwolf.com>
2020-02-21 14:19:17 -0600

    New Style

--- resources/views/newSections/head.blade.php
+++ resources/views/newSections/head.blade.php
@@ -42,9 +42,10 @@
 	<link rel="stylesheet" href="/css/settings.css" type="text/css" />
 	<link rel="stylesheet" href="/css/shapeDiver1.css" type="text/css" />
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/themes/classic.min.css"/> <!-- 'nano' theme -->
+	<link rel="stylesheet" href="/css/makUnique.css" type="text/css" />
 
 	<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-	
+
 	<link rel="stylesheet" href="/css/responsive.css" type="text/css" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 
