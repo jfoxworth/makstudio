fd3b0b2576c1e61e010bfa2ab94df1fbe9d35198
Joshua Foxworth <jfoxworth@cadwolf.com>
2020-02-21 16:57:35 -0600

    New Style

--- resources/views/newSections/landing/demoQuad.blade.php
+++ resources/views/newSections/landing/demoQuad.blade.php
@@ -5,14 +5,14 @@
 	<div class="full-screen section nopadding nomargin noborder ohidden" style="background: url('https://makstudio.s3.us-east-2.amazonaws.com/Homepage/fin_feature_wall_eldridge.jpg') bottom center no-repeat; background-size: cover;">
 
 	
-		<div class="container clearfix dark">
+		<div class="container-wide clearfix dark">
 
 
 
-			<div class="col_half cardHalf" style="background-image:url('https://makstudio.s3.us-east-2.amazonaws.com/Homepage/flower_solid_surface_feature_wall.jpg')">
+			<div class="col_half makCardHalf" style="background-image:url('https://makstudio.s3.us-east-2.amazonaws.com/Homepage/flower_solid_surface_feature_wall.jpg')">
 
 
-				<div class="vertical-middle" style="margin-top:-50px">
+				<div class="vertical-middle" style="margin-top:-50px; margin-left:10%;">
 
 					<div class="cardHalfTitle">Feature Walls</div>
 
@@ -26,10 +26,10 @@
 			</div>
 
 
-			<div class="col_half cardHalf col_last" style="background-image:url('https://makstudio.s3.us-east-2.amazonaws.com/Homepage/flower_solid_surface_feature_wall.jpg')">
+			<div class="col_half makCardHalf col_last" style="background-image:url('https://makstudio.s3.us-east-2.amazonaws.com/Homepage/flower_solid_surface_feature_wall.jpg')">
 
 
-				<div class="vertical-middle" style="margin-top:-50px">
+				<div class="vertical-middle" style="margin-top:-50px; margin-left:10%;">
 
 					<div class="cardHalfTitle">Reception Desks</div>
 
