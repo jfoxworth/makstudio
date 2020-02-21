7750534d81f1fdd9e774c54f1249daf2880cffbf
Joshua Foxworth <jfoxworth@cadwolf.com>
2020-02-21 16:47:06 -0600

    New Style

--- resources/views/newSections/landing/demoQuad.blade.php
+++ resources/views/newSections/landing/demoQuad.blade.php
@@ -9,48 +9,43 @@
 
 
 
-
-			<div class="row">
-
-				<div class="one_half cardHalf" style="background-image:url('https://makstudio.s3.us-east-2.amazonaws.com/Homepage/flower_solid_surface_feature_wall.jpg')">
+			<div class="card_half cardHalf" style="background-image:url('https://makstudio.s3.us-east-2.amazonaws.com/Homepage/flower_solid_surface_feature_wall.jpg')">
 
 
-					<div class="vertical-middle" style="margin-top:-50px">
+				<div class="vertical-middle" style="margin-top:-50px">
 
-						<div class="cardTitle">Feature Walls</div>
+					<div class="cardHalfTitle">Feature Walls</div>
 
-						<div class="cardText">From features to accents, our walls are designed to impress.</div>
+					<div class="cardHalfText">From features to accents, our walls are designed to impress.</div>
 
-						<button type="button" class="cardButton">Learn More</button>
-
-					</div>
-
-
-				</div>
-
-
-				<div class="one_half cardHalf col_last" style="background-image:url('https://makstudio.s3.us-east-2.amazonaws.com/Homepage/flower_solid_surface_feature_wall.jpg')">
-
-
-					<div class="one_half">
-
-						<div class="cardTitle">Reception Desks</div>
-
-						<div class="cardText">Make the most of your reception area with desks that fit perfectly</div>
-
-						<div class="cardButton">Learn More</div>
-
-					</div>
-
-
-					<div class="one_half col_last">
-
-					</div>
+					<button type="button" class="cardHalfButton">Learn More</button>
 
 				</div>
 
 
 			</div>
+
+
+			<div class="card_half cardHalf col_last" style="background-image:url('https://makstudio.s3.us-east-2.amazonaws.com/Homepage/flower_solid_surface_feature_wall.jpg')">
+
+
+				<div class="card_half">
+
+					<div class="cardHalfTitle">Reception Desks</div>
+
+					<div class="cardHalfText">Make the most of your reception area with desks that fit perfectly</div>
+
+					<div class="cardHalfButton">Learn More</div>
+
+				</div>
+
+
+				<div class="card_half col_last">
+
+				</div>
+
+			</div>
+
 
 		</div>
 
