217d8bc1f21300c9a42a104f984f3f28651f4d40
Joshua Foxworth <jfoxworth@cadwolf.com>
2020-03-11 23:09:10 -0500

    fixes

--- resources/views/newSections/landing/aboutLine.blade.php
+++ resources/views/newSections/landing/aboutLine.blade.php
@@ -12,7 +12,7 @@
 
 					<div class="aboutText">We design, build, and manufacture beautiful spaces, functional objects, furniture, and surfaces tailored to your unique vision and brand.</div>
 
-					<div class="aboutButton"><a href="/aboutUs">Learn More</a></div>
+					<div class="aboutButton"><a class="underLined" href="/aboutUs">Learn More</a></div>
 
 				</div>
 
@@ -22,19 +22,23 @@
 
 			<div class="col_half col_last center">
 
-				<div class="col_half aboutMe aboutImage" style="margin-left:25%">
+				<div class="col_half aboutMe aboutImage" style="margin-left:10%">
 
 					<img class="aboutImage" src='https://makstudio.s3.us-east-2.amazonaws.com/Homepage/Liz+and+Jose+Picture/jose_mak.jpg' height="130px" width="130px">
 
-					<div class="aboutName"><div class="boldName">Jose Aguilar</div>, AIA<br>Co-Founder<br>jose@makstudio.us</div>
+					<div class="aboutName underLined" style="height:100px">
+						<div class="boldName">Jose Aguilar</div>, AIA<br>
+						Co-Founder<br>
+						<a class="" href="mailto:jose@makstudio.us">jose@makstudio.us</a>
+					</div>
 
 				</div>
 
-				<div class="col_half col_last aboutMe aboutImage">
+				<div class="col_half col_last aboutMe aboutImage"  style="margin-left:25%">
 
 					<img class="aboutImage" src='https://makstudio.s3.us-east-2.amazonaws.com/Homepage/Liz+and+Jose+Picture/liz_mak.jpg'  height="130px" width="130px" >
 
-					<div class="aboutName"><div class="boldName">Liz Cordill</div>, AIA<br>Co-Founder<br>liz@makstudio.us</div>
+					<div class="aboutName underLined"><div class="boldName">Liz Cordill</div>, AIA<br>Co-Founder<br><a class="" href="mailto:liz@makstudio.us">liz@makstudio.us</a></div>
 
 				</div>
 
