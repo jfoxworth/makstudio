a2e53a68773ddf8d99ad38cf17c69fcf33c3d749
Joshua Foxworth <jfoxworth@cadwolf.com>
2020-03-24 11:00:12 -0500

    New Issues

--- resources/views/newSections/head.blade.php
+++ resources/views/newSections/head.blade.php
@@ -4,13 +4,13 @@
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
 	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-63540402-1"></script>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-89103879-1"></script>
 	<script>
 		window.dataLayer = window.dataLayer || [];
 		function gtag(){dataLayer.push(arguments);}
 		gtag('js', new Date());
 
-		gtag('config', 'UA-63540402-1');
+		gtag('config', 'UA-89103879-1');
 	</script>
 
 	<script>
